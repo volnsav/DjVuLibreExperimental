13b72567e47468e74a80048a79366cddfc222b25
Volodymyr <savchenko.volod@gmail.com>
2026-03-06 11:18:46 +0200

    Fix Linux linkage for DjVu message tests

--- tests/gtest/test_djvumessage.cpp
+++ tests/gtest/test_djvumessage.cpp
@@ -3,11 +3,6 @@
 #include "DjVuGlobal.h"
 #include "DjVuMessage.h"
 
-namespace DJVU {
-void DjVuMessageLookUpUTF8(char *msg_buffer, unsigned int buffer_size, const char *message);
-void DjVuMessageLookUpNative(char *msg_buffer, unsigned int buffer_size, const char *message);
-}
-
 TEST(DjVuMessageTest, ProgramNameRoundtripViaCApi)
 {
   const char *stored = djvu_programname("djvu_gtest_program");
@@ -41,18 +36,18 @@ TEST(DjVuMessageTest, SetProgramNameAndCreateFullAreCallable)
 TEST(DjVuMessageTest, CApiLookupFunctionsHandleSmallAndLargeBuffers)
 {
   char utf8_small[4] = {'x', 'x', 'x', 0};
-  DJVU::DjVuMessageLookUpUTF8(utf8_small, sizeof(utf8_small), "plain text");
+  ::DjVuMessageLookUpUTF8(utf8_small, sizeof(utf8_small), "plain text");
   EXPECT_STREQ("", utf8_small);
 
   char utf8_large[32] = {0};
-  DJVU::DjVuMessageLookUpUTF8(utf8_large, sizeof(utf8_large), "plain text");
+  ::DjVuMessageLookUpUTF8(utf8_large, sizeof(utf8_large), "plain text");
   EXPECT_STREQ("plain text", utf8_large);
 
   char native_small[4] = {'x', 'x', 'x', 0};
-  DJVU::DjVuMessageLookUpNative(native_small, sizeof(native_small), "plain text");
+  ::DjVuMessageLookUpNative(native_small, sizeof(native_small), "plain text");
   EXPECT_STREQ("", native_small);
 
   char native_large[32] = {0};
-  DJVU::DjVuMessageLookUpNative(native_large, sizeof(native_large), "plain text");
+  ::DjVuMessageLookUpNative(native_large, sizeof(native_large), "plain text");
   EXPECT_STREQ("plain text", native_large);
 }
